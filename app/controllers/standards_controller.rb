ce4f6257c7a4597a1c816f00c1e2553f8c857cc1
Aaqib Parvez <aaqib29292@gmail.com>
2016-03-04 12:04:35 +0530

    Partially done with standards CRUD opertions

--- app/controllers/standards_controller.rb
+++ app/controllers/standards_controller.rb
@@ -7,8 +7,17 @@ class StandardsController < ApplicationController
     @standard = Standard.find(params[:id])
   end
 
-  def create
+  def new
+    @standard = Standard.new
+  end
 
+  def create
+    @standard = Standard.new(standard_params)
+    if @standard.save
+      redirect_to standards_path
+    else
+      render :edit
+    end
   end
 
   def edit
@@ -16,10 +25,25 @@ class StandardsController < ApplicationController
   end
 
   def update
-
+    @standard = Standard.find(params[:id])
+    @standard.update_attributes(standard_params)
+    if @standard.save
+      redirect_to standards_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @standard = Standard.find(params[:id])
+    if @standard.destroy!
+      redirect_to standards_path
+    end
   end
+
+private
+  def standard_params
+    params.require(:standard).permit(:name)
+  end
+
 end
